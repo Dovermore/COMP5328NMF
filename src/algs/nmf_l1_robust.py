7f0ad80977c708eecf88fe0c241c98081240ac39
mdong-usyd <72338064+mdong-usyd@users.noreply.github.com>
2020-10-18 23:18:38 +1100

    Update L1

---
+++ src/algs/nmf_l1_robust.py
@@ -0,0 +1,39 @@
+# This file holds NMF algorithm with l1 norm loss
+# Author: Calvin Huang (zhuq9812) & Matthew Dong (mdon9995)
+
+import numpy as np
+from .base_nmf import BaseNmfEstimator
+
+
+class NmfL2Estimator(BaseNmfEstimator):
+    """
+    Base class for nmf robust l1 estimator. Uses sklearn skeleton for better coherence
+    with other parts of the codes.
+
+    For now only the function for updating D, R, loss should be updated
+    """
+
+    def get_next_D(self, X, D, R):
+        """
+        Compute the next value of D based on given input, D and R
+
+        This is the update rule for l1
+        """
+        next_D = D * ((X @ R.T) / (D @ R @ R.T))
+        return next_D
+
+    def get_next_R(self, X, D, R):
+        """
+        Compute the next value of R based on given input, D and R
+
+        This is the update rule for l1
+        """
+        next_R = max(0, R - ((R * (D.T @ D @ R)) / (abs(D.T @ D) @ R)) + ((R * (D.T @ X)) / (abs(D.T @ D) @ R)))
+        return next_R
+
+    @classmethod
+    def loss(cls, X, D, R):
+        """
+        use l1 loss
+        """
+        return np.sum(abs(X - D @ R))
