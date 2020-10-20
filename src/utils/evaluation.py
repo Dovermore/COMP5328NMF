ce83718b643ee9c8f71a993b0af87771e994f381
Dovermore <calvinyeye0627@gmail.com>
2020-10-20 15:24:42 +1100

    fix some issue from last commit

--- src/utils/evaluation.py
+++ src/utils/evaluation.py
@@ -1,5 +1,20 @@
+import numpy as np
+import pandas as pd
 import textwrap
 import itertools
+from collections import Counter
+from sklearn.cluster import KMeans
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import normalized_mutual_info_score
+
+
+def assign_cluster_label(X, Y):
+    kmeans = KMeans(n_clusters=len(set(Y))).fit(X)
+    Y_pred = np.zeros(Y.shape)
+    for i in set(kmeans.labels_):
+        ind = kmeans.labels_ == i
+        Y_pred[ind] = Counter(Y[ind]).most_common(1)[0][0] # assign label.
+    return Y_pred
 
 
 def make_grid_alg_kwargs(alg, **kwargs):
